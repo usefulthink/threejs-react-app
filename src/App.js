1ccbe889b8edaaa0ee0ad4abbb37cbeba20cf0da
Sofia Ribeiro <sribeiro@ubiwhere.pt>
2018-08-22 11:11:28 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
     var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+    renderer.setSize(window.innerWidth / 1.5, window.innerHeight / 1.5);
     document.body.appendChild(renderer.domElement);
 
     var loader = new THREE.ObjectLoader();
